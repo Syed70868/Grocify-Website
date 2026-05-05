40515bf50a35fc2fb3bbf6524cf73d2f74ec165f
・ ムしＩ・ <syedaliasghar355@gmail.com>
2026-05-05 21:20:06 +0500

    Values Section

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import NavBar from '../Navbar/NavBar';
 import Hero from '../Hero/Hero';
 import Category from '../Category/Category';
+import Values from '../Values/Values';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <NavBar />
       <Hero />
       <Category />
+      <Values />
     </div>
   );
 };
